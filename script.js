2d63fd143f65e9aa676ef5ff8b6e300221c21683
Chester <chesterdeveloper2000@gmail.com>
2020-09-13 22:00:56 +0800

    refactor

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-const menuBars = document.getElementById("menu-bars")
-const overlay = document.getElementById("overlay")
-const nav1 = document.getElementById("nav-1")
-const nav2 = document.getElementById("nav-2")
-const nav3 = document.getElementById("nav-3")
-const nav4 = document.getElementById("nav-4")
-const nav5 = document.getElementById("nav-5")
-const navItems = [nav1, nav2, nav3, nav4, nav5]
+const menuBars = document.getElementById("menu-bars");
+const overlay = document.getElementById("overlay");
+const nav1 = document.getElementById("nav-1");
+const nav2 = document.getElementById("nav-2");
+const nav3 = document.getElementById("nav-3");
+const nav4 = document.getElementById("nav-4");
+const nav5 = document.getElementById("nav-5");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
 
 // control animation navigation
 navAnimation = (direction1, direction2) => {
     navItems.forEach((nav, i) => {
-        nav.classList.replace(`slide-${direction1}-${i + 1}`, `slide-${direction2}-${i + 1}`)
-    })
-}
+        nav.classList.replace(
+            `slide-${direction1}-${i + 1}`,
+            `slide-${direction2}-${i + 1}`
+        );
+    });
+};
 
 toggleNav = () => {
     // toggle: menu bars open/closed
-    menuBars.classList.toggle("change")
+    menuBars.classList.toggle("change");
 
     // toggle: menu active
-    overlay.classList.toggle("overlay-active")
+    overlay.classList.toggle("overlay-active");
     if (overlay.classList.contains("overlay-active")) {
         // animate in our overlay
-        overlay.classList.replace("overlay-slide-left","overlay-slide-right")
+        overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
 
         // animate in nav items
-        navAnimation("out", "in")
+        navAnimation("out", "in");
     } else {
         // animate out our overlay
-        overlay.classList.replace("overlay-slide-right","overlay-slide-left")
+        overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
         // animate in nav items
-        navAnimation("in", "out")
+        navAnimation("in", "out");
     }
-}
-
+};
 
 // event listeners
-menuBars.addEventListener("click", toggleNav)
+menuBars.addEventListener("click", toggleNav);
 navItems.forEach((nav) => {
-    nav.addEventListener("click", toggleNav)
-})
+    nav.addEventListener("click", toggleNav);
+});
